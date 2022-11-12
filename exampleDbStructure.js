84566daf9987f70033428dee50369e928c557ede
Patryk Janicki <patrykjanicki@MacBook-Air-Patryk.local>
2022-11-12 14:36:09 +0100

    created pages structure, need to fix countries list display

---
+++ exampleDbStructure.js
@@ -0,0 +1,192 @@
+const db = {
+  slug: "poland",
+  country: "Polska",
+  areas: [
+    {
+      areaName: "Jura Północna",
+      rocksNames: [
+        {
+          rockName: "Skała niska",
+          routes: [
+            {
+              name: "Droga śmieszna",
+              grade: "6a",
+              type: "trad",
+              boltsCount: 0,
+              height: "30m",
+            },
+            {
+              name: "Droga kolorowa",
+              grade: "7a",
+              type: "sport",
+              boltsCount: 8,
+              height: "24m",
+            },
+          ],
+        },
+        {
+          rockName: "Skała wysoka",
+          routes: [
+            {
+              name: "Droga Patryka",
+              grade: "6a",
+              type: "trad",
+              boltsCount: 0,
+              height: "12m",
+            },
+            {
+              name: "Droga Sary",
+              grade: "7a",
+              type: "sport",
+              boltsCount: 8,
+              height: "30m",
+            },
+          ],
+        },
+        {
+          rockName: "Skała bombowa",
+          routes: [
+            {
+              name: "Droga łatwa",
+              grade: "6a",
+              type: "trad",
+              boltsCount: 0,
+              height: "15m",
+            },
+            {
+              name: "Droga średnia",
+              grade: "7a",
+              type: "sport",
+              boltsCount: 3,
+              height: "10m",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      areaName: "Jura Środkowa",
+      rocksNames: [
+        {
+          rockName: "Skała niebieska",
+          routes: [
+            {
+              name: "Droga balonowa",
+              grade: "5a",
+              type: "trad",
+              boltsCount: 0,
+              height: "30m",
+            },
+            {
+              name: "Droga szara",
+              grade: "6b",
+              type: "sport",
+              boltsCount: 12,
+              height: "24m",
+            },
+          ],
+        },
+        {
+          rockName: "Skała zielona",
+          routes: [
+            {
+              name: "Droga krzywa",
+              grade: "6a",
+              type: "trad",
+              boltsCount: 0,
+              height: "12m",
+            },
+            {
+              name: "Droga prosta",
+              grade: "7a",
+              type: "sport",
+              boltsCount: 8,
+              height: "30m",
+            },
+          ],
+        },
+        {
+          rockName: "Skała trudna",
+          routes: [
+            {
+              name: "Droga gumowa",
+              grade: "6a",
+              type: "trad",
+              boltsCount: 0,
+              height: "15m",
+            },
+            {
+              name: "Droga gładka",
+              grade: "7a",
+              type: "sport",
+              boltsCount: 3,
+              height: "10m",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      areaName: "Jura Południowa",
+      rocksNames: [
+        {
+          rockName: "Skała łatwa",
+          routes: [
+            {
+              name: "Droga na prawo",
+              grade: "5a",
+              type: "trad",
+              boltsCount: 0,
+              height: "30m",
+            },
+            {
+              name: "Droga na lewo",
+              grade: "6b",
+              type: "sport",
+              boltsCount: 12,
+              height: "24m",
+            },
+          ],
+        },
+        {
+          rockName: "Skała zielona",
+          routes: [
+            {
+              name: "Droga papierowa",
+              grade: "6a",
+              type: "trad",
+              boltsCount: 0,
+              height: "12m",
+            },
+            {
+              name: "Droga dla zawodowców",
+              grade: "7a",
+              type: "sport",
+              boltsCount: 8,
+              height: "30m",
+            },
+          ],
+        },
+        {
+          rockName: "Skała za mostem",
+          routes: [
+            {
+              name: "Droga od góry",
+              grade: "6a",
+              type: "trad",
+              boltsCount: 0,
+              height: "15m",
+            },
+            {
+              name: "Droga której nie ma",
+              grade: "7a",
+              type: "sport",
+              boltsCount: 3,
+              height: "10m",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
--- index.js
+++ index.js
@@ -1,55 +1,46 @@
 const express = require("express");
 const port = 3000;
-
+const path = require("path");
 const app = express();
 
+//view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname + "/views"));
 //logging in / registration
 
 app.get("/", (req, res) => {
-  res.send("strona rejestracji/logowania");
+  res.render("home");
 });
 
 //my profile - my ascents, settings
 
 app.get("/profile", (req, res) => {
-  res.send("mój profil - przejścia");
-});
-app.get("/profile/settings", (req, res) => {
-  res.send("mój profil - ustawienia");
+  res.render("profile");
 });
 
 //routes search
 
-app.get("/routes", (req, res) => {
-  res.send(`wyszukiwarka dróg wspinaczkowych`);
-});
-
-app.get("/routes/:country", (req, res) => {
-  const { country } = req.params;
+app.get("/countries/:name", (req, res) => {
+  const { name } = req.params;
   const countries = [
-    { slug: "poland", country: "Polska" },
-    { slug: "malta", country: "Malta" },
+    { slug: "poland", name: "Polska" },
+    { slug: "malta", name: "Malta" },
   ];
-  const findCountry = countries.find((x) => x.slug === country);
-  if (findCountry) {
-    res.send(`Kraj: ${findCountry.country}`);
-  } else {
-    res.send(`Nie ma takiego kraju`);
-  }
 
-  res.send(`wyszukiwarka dróg wspinaczkowych - ${findCountry?.country}`);
+  const findCountry = countries.find((x) => x.slug === name);
+
+  res.render("countries", {
+    name: findCountry?.name,
+    countries,
+  });
 });
 
-app.get("/routes/:country:/:area", (req, res) => {
-  res.send("wyszukiwarka dróg wspinaczkowych - kraj/strefa");
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
-app.get("/routes/:country:/:area/:rock", (req, res) => {
-  res.send("wyszukiwarka dróg wspinaczkowych - kraj/strefa/nazwa skały");
-});
-
-app.get("/kontakt", (req, res) => {
-  res.send("kontakt");
+app.get("*", (req, res) => {
+  res.render("errors/404");
 });
 
 app.listen(port);
